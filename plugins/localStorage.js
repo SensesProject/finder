d1b2cc4bdee1799726c044922e15fcc8ba6c2a68
z3to <6gqrbj@posteo.org>
2020-03-26 19:41:19 +0100

    Clean up

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -4,7 +4,7 @@ export default ({ store }) => {
   window.onNuxtReady(() => {
     const { dispatch, state } = store
     createPersistedState({
-    	// This id must be unique for each Finder instance on the same url
+      // This id must be unique for each Finder instance on the same url
       key: state.id.id,
       // These are the paths of the data that is stored in the localStorage
       paths: ['data.data', 'auth.token', 'data.status', 'data.date', 'auth.date', 'facets.facets', 'facets.date']
