79f9b0970ec29cbaeec2ec2264ac2c26fa3af50f
z3to <6gqrbj@posteo.org>
2020-03-26 19:36:51 +0100

    More documentation

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -4,10 +4,14 @@ export default ({ store }) => {
   window.onNuxtReady(() => {
     const { dispatch, state } = store
     createPersistedState({
+    	// This id must be unique for each Finder instance on the same url
       key: state.id.id,
+      // These are the paths of the data that is stored in the localStorage
       paths: ['data.data', 'auth.token', 'data.status', 'data.date', 'auth.date', 'facets.facets', 'facets.date']
     })(store)
-    dispatch('loadFacets')
-    dispatch('loadData')
+    // Load the facets (from the Google Sheet)
+    dispatch('loadFacets') // facets/loadFacets
+    // Then also load the data in parallel
+    dispatch('loadData') // data/loadData
   })
 }
